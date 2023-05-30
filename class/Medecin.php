03b6acb66162f4cd4682c4b0a289a86e87ade8ca
Eky26 <amanieze26@hotmail.com>
2023-05-30 12:08:01 +0000

    Create Medecin.php

---
+++ class/Medecin.php
@@ -0,0 +1,215 @@
+<?php
+include 'Action.php';
+class Medecin
+{
+    private $id;
+    private $medecinid;
+    private $nom;
+    private $prenom;
+    private $datenaissance;
+    private $email;
+    private $lieuhabitation;
+    private $photo;
+    private $createdby;
+    private $createdon;
+    private $status;
+    private $Action;
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserid()
+    {
+        return $this->medecinid;
+    }
+
+    /**
+     * @param mixed $medecinid
+     */
+    public function setUserid($medecinid)
+    {
+        $this->medecinid = $medecinid;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDatenaissance()
+    {
+        return $this->datenaissance;
+    }
+
+    /**
+     * @param mixed $datenaissance
+     */
+    public function setDatenaissance($datenaissance)
+    {
+        $this->datenaissance = $datenaissance;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLieuhabitation()
+    {
+        return $this->lieuhabitation;
+    }
+
+    /**
+     * @param mixed $lieuhabitation
+     */
+    public function setLieuhabitation($lieuhabitation)
+    {
+        $this->lieuhabitation = $lieuhabitation;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * @param mixed $photo
+     */
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedby()
+    {
+        return $this->createdby;
+    }
+
+    /**
+     * @param mixed $createdby
+     */
+    public function setCreatedby($createdby)
+    {
+        $this->createdby = $createdby;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedon()
+    {
+        return $this->createdon;
+    }
+
+    /**
+     * @param mixed $createdon
+     */
+    public function setCreatedon($createdon)
+    {
+        $this->createdon = $createdon;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAction()
+    {
+        return $this->Action;
+    }
+
+    /**
+     * @param mixed $Action
+     */
+    public function setAction($Action)
+    {
+        $this->Action = $Action;
+    }
+
+
+}
