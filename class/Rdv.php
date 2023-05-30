1d7cce3c9f4b5cb6ac2c452bd91f1a9c61d4959c
Eky26 <amanieze26@hotmail.com>
2023-05-30 15:10:14 +0000

    Create Rdv.php

---
+++ class/Rdv.php
@@ -0,0 +1,147 @@
+<?php
+include 'Action.php';
+class Rdv
+{
+    private $id;
+    private $rdvid;
+    private $userid;
+    private $specialitemedecinid;
+    private $createdby;
+    private $createdon;
+    private $status;
+    private $Action;
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRdvid()
+    {
+        return $this->rdvid;
+    }
+
+    /**
+     * @param mixed $rdvid
+     */
+    public function setRdvid($rdvid)
+    {
+        $this->rdvid = $rdvid;
+    }
+
+   /**
+     * @return mixed
+     */
+    public function getUserid()
+    {
+        return $this->userid;
+    }
+
+    /**
+     * @param mixed $userid
+     */
+    public function setUserid($userid)
+    {
+        $this->userid = $userid;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSpecialitemedecinid()
+    {
+        return $this->specialitemedecinid;
+    }
+
+    /**
+     * @param mixed $specialitemedecinid
+     */
+    public function setSpecialitemedecinid($specialitemedecinid)
+    {
+        $this->specialitemedecinid = $specialitemedecinid;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedby()
+    {
+        return $this->createdby;
+    }
+
+    /**
+     * @param mixed $createdby
+     */
+    public function setCreatedby($createdby)
+    {
+        $this->createdby = $createdby;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedon()
+    {
+        return $this->createdon;
+    }
+
+    /**
+     * @param mixed $createdon
+     */
+    public function setCreatedon($createdon)
+    {
+        $this->createdon = $createdon;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAction()
+    {
+        return $this->Action;
+    }
+
+    /**
+     * @param mixed $Action
+     */
+    public function setAction($Action)
+    {
+        $this->Action = $Action;
+    }
+
+
+}
